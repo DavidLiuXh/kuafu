48b34721413d7a29447568311d27dc36e589095b
liuwei <liuwei3-s@360.cn>
2018-04-17 14:28:10 +0800

    add exmaple for externallogger

--- examples/log/test_log.cc
+++ examples/log/test_log.cc
@@ -1,9 +1,12 @@
 #include <thread>
+#include <iostream>
+#include <sstream>
 
 #include "log/kflogger.h"
+#include "log/externallogger.h"
 
 void TestLog() {
-    kuafu::Logger::init(kuafu::Logger::Level::LL_INFO_LOG,
+    kuafu::Logger::init(kuafu::Logger::Level::LL_DEBUG_LOG,
                 nullptr,
                 "./test_log.log",
                 false);
@@ -13,12 +16,57 @@ void TestLog() {
     }
 }
 
-int main(int argc, char* argv[]) {
-    std::thread thread1(TestLog);
-    std::thread thread2(TestLog);
+void TestLogForMultiThread() {
+    std::thread thread_1(TestLog);
+    std::thread thread_2(TestLog);
 
-    thread1.join();
-    thread2.join();
+    thread_1.join();
+    thread_2.join();
+}
+
+class MyLogger: public kuafu::ExternalLogger {
+    public:
+        void ShowMsg(const char* msg) {
+            ExternalInfoLog("hello world!" << " | " << " ---- ");
+            ExternalDebugLog("hello world!");
+            ExternalWarnLog("hello world!");
+            ExternalErrorLog("hello world!" << " | " << " ---- ");
+        }
+};
+
+void TestLogForExternalLogger() {
+    kuafu::Logger::init(kuafu::Logger::Level::LL_DEBUG_LOG,
+                nullptr,
+                "./test_log.log",
+                false);
+
+    MyLogger my_logger;
+    my_logger.level = kuafu::ExternalLogger::Level::EL_WARNING_LOG;
+   
+    my_logger.infolog_delegate = [&](std::ostringstream&& os) {
+        INFO_LOG(os.str());
+    };
+    my_logger.debuglog_delegate = [&](std::ostringstream&& os) {
+        DEBUG_LOG(os.str());
+    };
+    my_logger.errlog_delegate = [&](std::ostringstream&& os) {
+        ERR_LOG(os.str());
+    };
+    my_logger.warninglog_delegate = [&](std::ostringstream&& os) {
+        WARNING_LOG(os.str());
+    };
+
+    my_logger.ShowMsg("");
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << "test log for multithread" << std::endl;
+    TestLogForMultiThread();
+    std::cout << "------------------------" << std::endl;
+
+    std::cout << "test external logger" << std::endl;
+    TestLogForExternalLogger();
+    std::cout << "------------------------" << std::endl;
 
     return 0;
 }
