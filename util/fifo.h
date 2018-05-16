e27072436dee48d4b4796e103559d18d48668793
liuwei <liuwei3-s@360.cn>
2018-05-16 10:22:18 +0800

    rm size_ mumber from fifo class

--- util/fifo.h
+++ util/fifo.h
@@ -21,43 +21,16 @@ class Fifo : public NonCopyableForAll {
       ~Fifo();
       
       void Add(FifoElementType msg);
-
-      /** Returns the first message available. It will wait if no
-       *  messages are available. If a signal interrupts the wait,
-       *  it will retry the wait. Signals can therefore not be caught
-       *  via getNext. If you need to detect a signal, use block
-       *  prior to calling getNext.
-       */
       FifoElementType GetNext();
-
-      /** Returns the next message available. Will wait up to
-       *  ms milliseconds if no information is available. If
-       *  the specified time passes or a signal interrupts the
-       *  wait, this method returns 0. This interface provides
-       *  no mechanism to distinguish between timeout and
-       *  interrupt.
-       */
       FifoElementType GetNext(unsigned int ms);
-
-      /** Returns the next message available and the queue status.
-       *  Will not wait if no information is available.
-       */
-
-      // delete all elements in the queue
       void Clear();
-
-      // Returns true if a message is available.
       bool MessageAvailable() const;
-
       bool Empty() const;
-
       unsigned int Size() const;
 
    private:
       typedef std::deque<FifoElementType> FifoQueueType;
       FifoQueueType fifo_;
-
-      unsigned long size_;
 
       mutable std::mutex mutex_;
       std::condition_variable condition_;
@@ -75,20 +48,19 @@ Fifo<Msg>::~Fifo() {
 template <class Msg>
 bool Fifo<Msg>::Empty() const {
     std::lock_guard<std::mutex> lock(mutex_); 
-    return size_ == 0;
+    return fifo_.empty();
 }
 
 template <class Msg>
 unsigned int Fifo<Msg>::Size() const {
     std::lock_guard<std::mutex> lock(mutex_);
-    return size_;
+    return static_cast<unsigned int>(fifo_.size());
 }
 
 template <class Msg>
 void Fifo<Msg>::Clear() {
     std::lock_guard<std::mutex> lock(mutex_);
     FifoQueueType().swap(fifo_);
-    size_ = 0;
     condition_.notify_one();
 }
 
@@ -96,7 +68,6 @@ template <class Msg>
 void Fifo<Msg>::Add(FifoElementType msg) {
     std::lock_guard<std::mutex> lock(mutex_);
     fifo_.push_back(msg);
-    ++size_;
     condition_.notify_one();
 }
 
@@ -110,7 +81,6 @@ typename Fifo<Msg>::FifoElementType Fifo<Msg>::GetNext() {
 
    FifoElementType first_message = fifo_.front();
    fifo_.pop_front();
-   --size_;
 
    return first_message;
 }
@@ -122,13 +92,12 @@ typename Fifo<Msg>::FifoElementType Fifo<Msg>::GetNext(unsigned int internal_ms)
     auto now = std::chrono::system_clock::now();
 
     std::unique_lock<std::mutex> lk(mutex_);
-    bool signaled = condition_.wait_until(lk, now + internal_ms * std::chrono::milliseconds(1), [this]() {
+    bool signaled = condition_.wait_until(lk, now + std::chrono::milliseconds(internal_ms), [this]() {
                 return !fifo_.empty();
                 });
     if (signaled) {
         first_message = fifo_.front();
         fifo_.pop_front();
-        --size_;
     }
 
     return first_message;
