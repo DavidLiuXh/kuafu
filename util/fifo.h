5e2bfe06641afea6b2e8c25c49f59264321601e4
liuwei <liuwei3-s@360.cn>
2018-05-16 16:16:04 +0800

    tidy up codes for fifo

--- util/fifo.h
+++ util/fifo.h
@@ -11,100 +11,107 @@
 
 namespace kuafu {
 
-template <class Msg>
+template <class T>
 class Fifo : public NonCopyableForAll {
- public:
-     typedef typename std::shared_ptr<Msg> FifoElementType;
-
  public:
       Fifo();
       ~Fifo();
       
-      void Add(FifoElementType msg);
-      FifoElementType GetNext();
-      FifoElementType GetNext(unsigned int ms);
+      void Add(const T& msg);
+      void Add(T&& msg);
+      T GetNext();
+      T GetNext(unsigned int ms);
       void Clear();
       bool MessageAvailable() const;
       bool Empty() const;
       unsigned int Size() const;
 
    private:
-      typedef std::deque<FifoElementType> FifoQueueType;
+      typedef std::deque<T> FifoQueueType;
       FifoQueueType fifo_;
 
       mutable std::mutex mutex_;
       std::condition_variable condition_;
 };
 
-template <class Msg>
-Fifo<Msg>::Fifo() {
+template <class T>
+Fifo<T>::Fifo()
+:fifo_() {
 }
 
-template <class Msg>
-Fifo<Msg>::~Fifo() {
+template <class T>
+Fifo<T>::~Fifo() {
    Clear();
 }
 
-template <class Msg>
-bool Fifo<Msg>::Empty() const {
+template <class T>
+bool Fifo<T>::Empty() const {
     std::lock_guard<std::mutex> lock(mutex_); 
     return fifo_.empty();
 }
 
-template <class Msg>
-unsigned int Fifo<Msg>::Size() const {
+template <class T>
+unsigned int Fifo<T>::Size() const {
     std::lock_guard<std::mutex> lock(mutex_);
     return static_cast<unsigned int>(fifo_.size());
 }
 
-template <class Msg>
-void Fifo<Msg>::Clear() {
+template <class T>
+void Fifo<T>::Clear() {
     std::lock_guard<std::mutex> lock(mutex_);
     FifoQueueType().swap(fifo_);
     condition_.notify_one();
 }
 
-template <class Msg>
-void Fifo<Msg>::Add(FifoElementType msg) {
+template <class T>
+void Fifo<T>::Add(const T& msg) {
     std::lock_guard<std::mutex> lock(mutex_);
     fifo_.push_back(msg);
     condition_.notify_one();
 }
 
-template <class Msg>
-typename Fifo<Msg>::FifoElementType Fifo<Msg>::GetNext() {
-   std::unique_lock<std::mutex> lk(mutex_);
+template <class T>
+void Fifo<T>::Add(T&& msg) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    fifo_.push_back(std::move(msg));
+    condition_.notify_one();
+}
 
+template <class T>
+T Fifo<T>::GetNext() {
+   std::unique_lock<std::mutex> lk(mutex_);
    condition_.wait(lk, [this]() {
                return !fifo_.empty();
                });
 
-   FifoElementType first_message = fifo_.front();
+   T first_message(std::move(fifo_.front()));
    fifo_.pop_front();
 
    return first_message;
 }
 
-template <class Msg>
-typename Fifo<Msg>::FifoElementType Fifo<Msg>::GetNext(unsigned int internal_ms) {
-    FifoElementType first_message;
+template <class T>
+T Fifo<T>::GetNext(unsigned int internal_ms) {
+    T first_message;
 
     auto now = std::chrono::system_clock::now();
 
     std::unique_lock<std::mutex> lk(mutex_);
-    bool signaled = condition_.wait_until(lk, now + std::chrono::milliseconds(internal_ms), [this]() {
+    bool signaled = condition_.wait_until(lk,
+                now + std::chrono::milliseconds(internal_ms),
+                [this]() {
                 return !fifo_.empty();
                 });
     if (signaled) {
-        first_message = fifo_.front();
+        first_message = std::move(fifo_.front());
         fifo_.pop_front();
     }
 
     return first_message;
 }
 
-template <class Msg>
-bool Fifo<Msg>::MessageAvailable() const {
+template <class T>
+bool Fifo<T>::MessageAvailable() const {
     std::lock_guard<std::mutex> lock(mutex_);
     return !fifo_.empty();
 }
